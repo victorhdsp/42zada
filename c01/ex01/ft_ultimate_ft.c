5bed3e0315fe1d3368753cefacf662e67eaa493f
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-07-14 22:06:19 -0300

    exercicios 00 e 01, da lista c01

---
+++ c01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkt <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/14 21:59:23 by lkt               #+#    #+#             */
+/*   Updated: 2024/07/14 22:05:35 by lkt              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	int	pointer = 0;
+	int	*pointer1 = &pointer;
+	int	**pointer2 = &pointer1;
+	int	***pointer3 = &pointer2;
+	int	****pointer4 = &pointer3;
+	int	*****pointer5 = &pointer4;
+	int	******pointer6 = &pointer5;
+	int	*******pointer7 = &pointer6;
+	int	********pointer8 = &pointer7;
+	
+	ft_ultimate_ft(&pointer8);
+	printf("Esperado: 42, Resultado: %d", pointer);
+}*/
